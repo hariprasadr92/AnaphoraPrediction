e4a4d8e1c95e4d47a731ee2a9a8aa781fa589f9e
Hariprasad R <hariprasadr92@gmail.com>
2020-10-30 06:13:04 -0400

    tregex wip

--- src/main/java/com/uo/nlp4se/assg2/ConstiuencyParsing.java
+++ src/main/java/com/uo/nlp4se/assg2/ConstiuencyParsing.java
@@ -4,6 +4,8 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+import edu.stanford.nlp.trees.tregex.TregexPattern;
 
 import java.util.*;
 
@@ -14,21 +16,43 @@ public class ConstiuencyParsing {
     StanfordCoreNLP pipeline = Pipeline.getPipeline();
     // build annotation for a review
     Annotation annotation =
-        new Annotation("When in normal operation, the output shall be the result of the equation.");
+        new Annotation("It  is  retarded  under  certain  conditions,  none  of  which,  however,  appear  to  have  been  present  in  this  case.");
     // annotate
     pipeline.annotate(annotation);;
     // get tree
     Tree tree =
         annotation.get(CoreAnnotations.SentencesAnnotation.class).get(0).get(TreeCoreAnnotations.TreeAnnotation.class);
     System.out.println(tree.pennString());
+    
     Set<Constituent> treeConstituents = tree.constituents(new LabeledScoredConstituentFactory());
     for (Constituent constituent : treeConstituents) {
       if (constituent.label() != null 
     		  && (constituent.label().toString().equals("VP") || constituent.label().toString().equals("NP"))
     		  ) {
-        System.err.println("found constituent: "+constituent.toString());
-        System.err.println(tree.getLeaves().subList(constituent.start(), constituent.end()+1));
+        //System.err.println("found constituent: "+constituent.toString());
+        //System.err.println(tree.getLeaves().subList(constituent.start(), constituent.end()+1));
       }
     }
+    //tregex code 
+    String regex = "NP !< NP";
+    TregexPattern p = TregexPattern.compile(regex);
+    TregexMatcher m = p.matcher(tree);
+    
+    while (m.find()) {
+    	Tree subtree = m.getMatch();
+        Set<Constituent> treeConstituentss = subtree.constituents(new LabeledScoredConstituentFactory());
+        for (Constituent constituent : treeConstituentss) {
+            if (constituent.label() != null 
+          		  //&& (constituent.label().toString().equals("VP") || constituent.label().toString().equals("NP"))
+          		  ) {
+              //System.err.println("found constituent: "+constituent.toString());
+              System.err.println(tree.getLeaves());
+            }
+          }
+        
+    	  // System.out.println(m.getMatch().constituents());
+    }
+
+
   }
 }
